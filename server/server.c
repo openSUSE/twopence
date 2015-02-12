openSUSE/twopence

commit b3a94375f604466255e7eec334d0bd446add0939

server: actually use the provided file mode during file creation
Signed-off-by: Olaf Kirch <okir@suse.de>

server/server.c
@@ -173,13 +173,13 @@ server_open_file_as(const char *username, const char *filename, unsigned int fil
 
 	/* We may want to have the client specify the file mode as well */
 	if (!strcmp(username, "root")) {
-		fd = open(filename, oflags, 0660);
+		fd = open(filename, oflags, filemode);
 		if (fd < 0)
 			*status = errno;
 	} else {
 		if (!server_change_hats_temporarily(user, &saved_ids, status))
 			return -1;
-		fd = open(filename, oflags, 0660);
+		fd = open(filename, oflags, filemode);
 		if (fd < 0)
 			*status = errno;
 
@@ -188,6 +188,13 @@ server_open_file_as(const char *username, const char *filename, unsigned int fil
 
 	if (fd < 0)
 		return -1;
+
+	if (fchmod(fd, filemode) < 0) {
+		*status = errno;
+		twopence_log_error("failed to change file mode \"%s\" to 0%o: %m", filename, filemode);
+		close(fd);
+		return -1;
+	}
 
 	if (fstat(fd, &stb) < 0) {
 		*status = errno;

commit c89b32048afece1b80866671aa0aeb95140f6f7b

Fix a bug introduced by commit b3a94375f604466255e7eec334d0bd446add0939
Thanks to this patch, trying to extract /etc would change its permissions to 0600 :-(

Signed-off-by: Olaf Kirch <okir@suse.de>

server/server.c
@@ -189,13 +189,6 @@ server_open_file_as(const char *username, const char *filename, unsigned int fil
 	if (fd < 0)
 		return -1;
 
-	if (fchmod(fd, filemode) < 0) {
-		*status = errno;
-		twopence_log_error("failed to change file mode \"%s\" to 0%o: %m", filename, filemode);
-		close(fd);
-		return -1;
-	}
-
 	if (fstat(fd, &stb) < 0) {
 		*status = errno;
 		twopence_log_error("failed to stat \"%s\": %m", filename);
@@ -205,6 +198,12 @@ server_open_file_as(const char *username, const char *filename, unsigned int fil
 	if (!S_ISREG(stb.st_mode)) {
 		twopence_log_error("%s: not a regular file\n", filename);
 		*status = EISDIR;
+		close(fd);
+		return -1;
+	}
+	if (oflags != O_RDONLY && fchmod(fd, filemode) < 0) {
+		*status = errno;
+		twopence_log_error("failed to change file mode \"%s\" to 0%o: %m", filename, filemode);
 		close(fd);
 		return -1;
 	}