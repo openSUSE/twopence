a9db2adf2030620a971e2a1ef78eb2553ae0450f
Olaf Kirch <okir@suse.de>
2015-04-01 17:53:31 +0200

    moved protocol.[hc] from server to library
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- library/protocol.c
+++ library/protocol.c
@@ -1,17 +1,17 @@
 /*
  * protocol and transaction routines for test server.
- * 
+ *
  * Copyright (C) 2014-2015 SUSE
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 2.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -44,7 +44,7 @@
 #include <ctype.h>
 #include <limits.h>
 
-#include "server.h"
+#include "protocol.h"
 
 
 /*
@@ -193,7 +193,6 @@ twopence_protocol_dissect_string(twopence_buf_t *bp, char *stringbuf, unsigned i
 		;
 	bp->head += n;
 
-	TRACE("%s()=\"%s\"\n", __func__, stringbuf);
 	return true;
 }
 
