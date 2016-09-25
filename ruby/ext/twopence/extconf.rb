690c8bac8e98578aae721c8adacbeedb346b62c6
Dario Maiocchi <MalloZup@users.noreply.github.com>
2016-09-25 19:27:32 +0200

    comment

--- ruby/ext/twopence/extconf.rb
+++ ruby/ext/twopence/extconf.rb
@@ -6,7 +6,7 @@ dir_config(extension_name)
 # this are not needed for Fedora and co, since are from rpms env vars.
 #FIXME: maybe detect wich OS is running and make this for openSUSE else not
 
-$LDFLAGS = '/usr/lib64/libtwopence.so' + ' '  + $LDFLAGS
+# $LDFLAGS = '/usr/lib64/libtwopence.so' + ' '  + $LDFLAGS
 # $CFLAGS = ENV['CFLAGS'] + ' ' + $CFLAGS
 have_library('twopence', 'twopence_target_new')
 create_makefile(extension_name)
