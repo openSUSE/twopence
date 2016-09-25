6b6fe0d76c097b331788e7739fa9782c3ae6e2ac
Dario Maiocchi <MalloZup@users.noreply.github.com>
2016-09-25 13:20:58 +0200

    update extension

--- ruby/ext/twopence/extconf.rb
+++ ruby/ext/twopence/extconf.rb
@@ -3,7 +3,10 @@ require 'mkmf'
 extension_name = 'twopence'
 dir_config(extension_name)
 
-$LDFLAGS = ENV['LDFLAGS'] + ' ' + $LDFLAGS
-$CFLAGS = ENV['CFLAGS'] + ' ' + $CFLAGS
+# this are not needed for Fedora and co, since are from rpms env vars.
+#FIXME: maybe detect wich OS is running and make this for openSUSE else not
+
+# $LDFLAGS = ENV['LDFLAGS'] + ' ' + $LDFLAGS
+# $CFLAGS = ENV['CFLAGS'] + ' ' + $CFLAGS
 have_library('twopence', 'twopence_target_new')
 create_makefile(extension_name)
