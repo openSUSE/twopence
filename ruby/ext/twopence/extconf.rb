da3513e138daebc133bb3b1d9ce74d6d27f4ebfa
Dario Maiocchi <MalloZup@users.noreply.github.com>
2016-09-26 10:43:20 +0200

    debug and linking?

--- ruby/ext/twopence/extconf.rb
+++ ruby/ext/twopence/extconf.rb
@@ -1,12 +1,13 @@
 require 'mkmf'
 
 extension_name = 'twopence'
-dir_config(extension_name)
+dir_config(extension_name, '/usr/include/' , '/usr/lib64/')
 
 # this are not needed for Fedora and co, since are from rpms env vars.
 #FIXME: maybe detect wich OS is running and make this for openSUSE else not
 
 # $LDFLAGS = '/usr/lib64/libtwopence.so' + ' '  + $LDFLAGS
 # $CFLAGS = ENV['CFLAGS'] + ' ' + $CFLAGS
-have_library('twopence', 'twopence_target_new') or raise
+
+have_library('twopence', 'twopence_target_new') or raise "Requirements library not satisfied"
 create_makefile(extension_name)
