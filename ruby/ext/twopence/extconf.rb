29acc08f78429beec02fb124fb0f732a0dcf0fb3
Dario Maiocchi <MalloZup@users.noreply.github.com>
2016-09-26 13:49:58 +0200

    fxi

--- ruby/ext/twopence/extconf.rb
+++ ruby/ext/twopence/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 
 extension_name = 'twopence'
-dir_config(extension_name, '/usr/include/' , '/usr/lib64/')
+dir_config(extension_name, '/usr/include/' , '/usr/lib/x86_64-linux-gnu')
 
 # this are not needed for Fedora and co, since are from rpms env vars.
 #FIXME: maybe detect wich OS is running and make this for openSUSE else not
