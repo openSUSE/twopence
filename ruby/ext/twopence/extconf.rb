cb5f5611caca92d5f83ab946ac739ed0d54c7166
Eric Bischoff <ebischoff@nerim.net>
2016-09-26 16:47:23 +0200

    Multi-platform compile

--- ruby/ext/twopence/extconf.rb
+++ ruby/ext/twopence/extconf.rb
@@ -5,5 +5,5 @@ dir_config(extension_name)
 
 $LDFLAGS = ENV['LDFLAGS'] + ' ' + $LDFLAGS
 $CFLAGS = ENV['CFLAGS'] + ' ' + $CFLAGS
-have_library('twopence', 'twopence_target_new')
+have_library('twopence', 'twopence_target_new') or raise
 create_makefile(extension_name)
